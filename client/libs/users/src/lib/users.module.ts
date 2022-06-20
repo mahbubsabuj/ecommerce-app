eafddcf471706f87947017fb7367301eb03fa1ae
mahbubsabuj <mahbubsabuj3@gmail.com>
2022-06-20 18:53:22 +0600

    style: :lipstick: Add styles to LoginComponent

--- client/libs/users/src/lib/users.module.ts
+++ client/libs/users/src/lib/users.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { InputTextModule } from 'primeng/inputtext';
 
-const usersRoutes: Route[] = [
-  { path: 'login', component: LoginComponent },
-];
+const usersRoutes: Route[] = [{ path: 'login', component: LoginComponent }];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(usersRoutes)],
+  imports: [CommonModule, RouterModule.forChild(usersRoutes), InputTextModule],
   declarations: [LoginComponent],
 })
 export class UsersModule {}
