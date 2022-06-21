adc47720b9e07fedea1d95658002601df413b8a2
mahbubsabuj <mahbubsabuj3@gmail.com>
2022-06-21 23:16:19 +0600

    style: :lipstick: Add Animation to the BannerComponent

--- client/libs/ui/src/lib/banner/banner.component.ts
+++ client/libs/ui/src/lib/banner/banner.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'client-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss'],
 })
-export class BannerComponent {
-  
-}
+export class BannerComponent {}
